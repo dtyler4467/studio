dac7c217bdb60679a6fc13b85bc64ae6aabe0706
Dorrell Tyler <dtyler4467@gmail.com>
2025-09-07 22:18:19 +0000

    still not working

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,18 +1,24 @@
-import { initializeApp, getApps, getApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { initializeApp, getApps, getApp, FirebaseApp } from "firebase/app";
+import { getAuth, Auth } from "firebase/auth";
 
 const firebaseConfig = {
-  projectId: "logiflow-8oju4",
-  appId: "1:719952558782:web:dc7dddf4ec6ba35baf7cef",
-  storageBucket: "logiflow-8oju4.firebasestorage.app",
-  apiKey: "AIzaSyDxS__aq2vbL6mSV4xsaLWnWCh7oyFu8EE",
-  authDomain: "logiflow-8oju4.firebaseapp.com",
-  messagingSenderId: "719952558782",
-  measurementId: "G-XXXXXXXXXX"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 };
 
-// Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-const auth = getAuth(app);
+let app: FirebaseApp;
+let auth: Auth;
+
+if (getApps().length === 0) {
+  app = initializeApp(firebaseConfig);
+} else {
+  app = getApp();
+}
+
+auth = getAuth(app);
 
 export { app, auth };
