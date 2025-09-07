f355705f96797ec60b1745a6aa2aa3bba5813b78
Dorrell Tyler <dtyler4467@gmail.com>
2025-09-07 22:20:43 +0000

    signup  registartion process still not working please fix errors

--- next.config.ts
+++ next.config.ts
@@ -24,6 +24,14 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  env: {
+    NEXT_PUBLIC_FIREBASE_PROJECT_ID: 'logiflow-8oju4',
+    NEXT_PUBLIC_FIREBASE_APP_ID: '1:719952558782:web:dc7dddf4ec6ba35baf7cef',
+    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: 'logiflow-8oju4.firebasestorage.app',
+    NEXT_PUBLIC_FIREBASE_API_KEY: 'AIzaSyDxS__aq2vbL6mSV4xsaLWnWCh7oyFu8EE',
+    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: 'logiflow-8oju4.firebaseapp.com',
+    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: '719952558782',
+  },
 };
 
 export default nextConfig;
