8ac6940387ffc898e1a7e3f2ecdddc3060bb8f47
Dorrell Tyler <dtyler4467@gmail.com>
2025-09-08 18:50:42 +0000

    calendar still has errors , i cant see a full calender year

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -59,6 +59,9 @@ function Calendar({
         IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
         IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
       }}
+       captionLayout="dropdown-nav"
+       fromYear={1900}
+       toYear={2100}
       {...props}
     />
   )
