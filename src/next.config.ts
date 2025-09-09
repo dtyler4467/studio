a791a8b4a3f386511adb4d42d0a987831a95aa62
Dorrell Tyler <dtyler4467@gmail.com>
2025-09-09 02:32:49 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- src/next.config.ts
+++ src/next.config.ts
@@ -48,6 +48,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.weather.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   env: {
