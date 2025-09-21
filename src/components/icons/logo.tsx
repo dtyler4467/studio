c0be2af6ecfeedcffc164ad9c45bebef7d5d6277
Dorrell Tyler <dtyler4467@gmail.com>
2025-09-21 19:01:40 +0000

    logo for propantheon not showing correctyl

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -1,15 +1,25 @@
+
 import type { SVGProps } from "react";
-import Image from 'next/image';
 
 export function Logo(props: SVGProps<SVGSVGElement>) {
   return (
-    <Image 
-        src="https://picsum.photos/seed/propantheon/160/32" 
-        alt="ProPantheon Logo" 
-        width={120} 
-        height={24}
-        className="h-auto"
-        {...props}
-    />
+    <svg 
+      xmlns="http://www.w3.org/2000/svg" 
+      viewBox="0 0 160 32" 
+      width="120" 
+      height="24"
+      {...props}
+    >
+      <text 
+        x="0" 
+        y="24" 
+        fontFamily="Space Grotesk, sans-serif" 
+        fontSize="24" 
+        fontWeight="bold" 
+        fill="hsl(var(--primary))"
+      >
+        ProPantheon
+      </text>
+    </svg>
   );
 }
